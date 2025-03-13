9078c6f639a38147951f81720b9d3e15d97bd828
Wambuarack <shedrackwambua40@gmail.com>
2025-03-13 12:48:50 +0300

    s

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('employees/', views.employee_list),
+    path('employees/', views.employee_list, name='create_employee'),
     path('employee/<int:pk>/', views.employee_detail)
 ]
